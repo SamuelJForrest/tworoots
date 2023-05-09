0f2b3a139fbe582740b710c678bc0aa2ac8351bf
Sam Forrest <samforrest@live.co.uk>
2023-05-09 21:30:48 +0100

    INIT: Initial Commit

---
+++ tworoots/tworoots/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_1fg3f8(j5aqyx4ijujugam%hcdoona1@=)p_b2!p-$0=ctai2"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
